edd5fae3c107b15bb00aebb2854038dced74496b
sidHathi <sidhathi20@gmail.com>
2021-11-16 14:04:08 -0500

    Added bonds to final index

--- Assets/Scripts/Core/Constants.cs
+++ Assets/Scripts/Core/Constants.cs
@@ -96,7 +96,8 @@ namespace sib
             new int[] { 0, 1, 3, 5 },
             new int[] { 2, 4, 6, 7 },
             new int[] { 0, 1, 2, 6 },
-            new int[] { 3, 4, 5, 7 }
+            new int[] { 3, 4, 5, 7 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }
         };
 
         public static int[][] planarIndices = new int[][] {
