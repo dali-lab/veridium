eb495d30e3dc96fe2bf386414127c74eca48d266
bkidman20 <71234963+bkidman20@users.noreply.github.com>
2022-01-19 15:17:23 -0500

    button func

--- Assets/Scripts/Misc/ButtonChangeScene.cs
+++ Assets/Scripts/Misc/ButtonChangeScene.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonChangeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  public void playGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Back()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
