476d046dd9458e7822e1475e9a62bc95b6f3faf2
Andy Kotz <andy.lyme@mac.com>
2022-01-27 15:00:27 -0500

    Renaming

--- Assets/Scripts/Interaction/RayCaster.cs
+++ Assets/Scripts/Interaction/RayCaster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SIB_Interaction{
+namespace Veridium_Interaction{
 
     public class RayCaster : MonoBehaviour
     {
