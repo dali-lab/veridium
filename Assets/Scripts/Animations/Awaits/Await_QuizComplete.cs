195160d325b4eac6849228d599fb0dc6d19bfc0c
alexander-hm <alexanderhuang5@gmail.com>
2023-02-21 17:50:15 -0500

    Update Await_QuizComplete.cs

--- Assets/Scripts/Animations/Awaits/Await_QuizComplete.cs
+++ Assets/Scripts/Animations/Awaits/Await_QuizComplete.cs
@@ -102,8 +102,14 @@ namespace Veridium_Animation{
             int i = 0;
             Vector3[] ABC = new Vector3[3];
             foreach (GameObject atom in answer)
-            {
-                Vector3 atomPos = Vector3.zero; // structureBuilder.GetCoordinateAtAtom(atom.GetComponent<Atom>());
+            {}  
+                Vector3 atomPos = Vector3.zero;
+                foreach (Atom a in structureBuilder.crystal.atoms.Values)
+                {
+                    if (a.drawnObjectatom == atom) {
+                        atomPos = structureBuilder.GetCoordinateAtAtom(a);
+                    }
+                }
                 // Compute planar equation
                 if (i < 3)
                 {
