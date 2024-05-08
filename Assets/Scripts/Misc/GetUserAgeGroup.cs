179e015caa772771c34a6c279268da4aa319eb78
Julian Arnold <julian.arnold@student.hpi.de>
2024-05-08 18:24:45 +0200

    only do on headset

--- Assets/Scripts/Misc/GetUserAgeGroup.cs
+++ Assets/Scripts/Misc/GetUserAgeGroup.cs
@@ -8,7 +8,9 @@ public class GetUserAgeGroup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        #if !UNITY_EDITOR
         UserAgeCategory.Get();
+        #endif
     }
     
 }
