a2fb50302b5e5742fa3da6fac6b101413740a4ef
Julian Arnold <julian.arnold@student.hpi.de>
2024-05-08 18:22:27 +0200

    get user age category

---
+++ Assets/Scripts/Misc/GetUserAgeGroup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Platform;
+using UnityEngine;
+
+public class GetUserAgeGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UserAgeCategory.Get();
+    }
+    
+}
